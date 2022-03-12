454ef843b378302e08024239f04adbc9cae4e7e3
Zelipha Wambui <83716522+Zelipha@users.noreply.github.com>
2022-03-12 10:40:40 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+'''
+    Implementation of the Review class
+'''
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import String, Column, ForeignKey
+import os
 
 
 class Review(BaseModel, Base):
-    """ Review class to store review information """
-    __tablename__ = 'review'
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    '''
+        Implementation for the Review.
+    '''
+    __tablename__ = "reviews"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
