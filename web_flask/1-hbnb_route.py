b0608584424247a8606c491e8afcd7b3aaa1d88a
nandweza <allannandweza@gmail.com>
2022-04-08 07:00:42 -0700

    added task 1 & 2

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """displays a string at the / route."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays a string at the /hbnb route."""
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """displays a string at / route."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays a string at /hbnb route."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """displays "C" followed by the value
+    of the text variable at /c/<text> route."""
+    new = text.replace('_', ' ')
+    return 'C %s' % new
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
