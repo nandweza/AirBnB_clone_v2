2637ead501c3ca429f77c8ce8b213f050485eda4
nandweza <allannandweza@gmail.com>
2022-04-09 06:08:02 -0700

    fixed code

--- models/city.py
+++ models/city.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ The city class, contains state ID and name
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
     __tablename__ = 'cities'
-    state_id = Column(string(60), nullable=False, ForeignKey('states.id'))
-    name = Column(string(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
 
     places = relationship('Place', backref='cities',
                           cascade='all, delete-orphan')
--- models/state.py
+++ models/state.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
+import models
+from models.base_model import BaseModel, Base
 from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """ State class
+    Attributes:
+        name: input name
+    """
+
     __tablename__ = 'states'
-    name = Column(string(128), nullable=False)
+    name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship('City', backref='state',
